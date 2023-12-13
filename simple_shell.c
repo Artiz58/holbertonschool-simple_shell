d304d15360a962ed7a59c445589446a7d41252f5
Artiz58 <artiz58@gmail.com>
2023-12-13 15:08:37 -0400

    Modifying simple shell file

--- simple_shell.c
+++ simple_shell.c
@@ -1,38 +1,38 @@
-#include "shell.h"
-/**
- * main - Simple shell program
- *
- * This is a simple shell program that allows the user to enter commands.
- * It supports executing commands and the "which" command for checking file
- * existence.
- *
- * Return: 0 on success, non-zero on failure.
- */
-int main(void)
-{
-	char *buffer = NULL;
-	size_t buffer_size = 0;
-	ssize_t length;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-	while (1)
-	{
-		printf("($) ");
-		getline(&buffer, &buffer_size, stdin);
+#define BUFFER_SIZE 128
 
-		length = strlen(buffer);
-		if (length > 0 && buffer[length - 1] == '\n')
-		{
-			buffer[length - 1] = '\0';
-		}
+void prompt() {
+  printf("($) ");
+}
 
-		if (strcmp(buffer, "exit") == 0)
-		{
-			break;
-		}
+extern char **environ; // Declare the external variable
 
-		parse_and_execute(buffer);
-	}
-	free(buffer);
+int main() {
+  char buffer[BUFFER_SIZE];
+  char *command;
 
-	return (0);
+  while (1) {
+    prompt();
+
+    if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+      printf("\n");
+      break;
+    }
+
+    command = strtok(buffer, " \n");
+
+    if (!command || strlen(command) == 0) {
+      continue;
+    }
+
+    if (execve(command, NULL, environ) == -1) { // Use environ instead of envp
+      printf("'%s': No such file or directory\n", command);
+    }
+  }
+
+  return 0;
 }
