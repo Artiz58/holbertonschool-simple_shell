543d4e9811dd16b751efb86c1657e7d9a9dff0b8
Artiz58 <artiz58@gmail.com>
2023-12-13 19:16:22 -0400

    Simple Shell modifications

--- simple_shell.c
+++ simple_shell.c
@@ -2,40 +2,41 @@
 
 int main(void)
 {
-        size_t buf_size = 0;
-        char *buf = NULL;
-        char *token;
-        int status, i = 0;
-        char **array;
+	size_t buf_size = 0;
+	char *buf = NULL;
+	char *token;
+	int status, i = 0;
+	char **array;
 
-        pid_t child_pid;
+	pid_t child_pid;
 
-        while (1)
-        {
-                printf("($) ");
-                getline(&buf, &buf_size, stdin);
-                token = strtok(buf, "\t\n");
-                array = malloc(sizeof(char *) * 1024);
+	while (1)
+	{
+	printf("($) ");
+	getline(&buf, &buf_size, stdin);
+	token = strtok(buf, "\t\n");
+	array = malloc(sizeof(char *) * 1024);
 
-                while (token)
-                {
-                        array[i] = token;
-                        token = strtok(NULL, "\t\n");
-                        i++;
-                }
-                        array[i] = NULL;
-                        child_pid = fork();
+	while (token)
+	{
+		array[i] = token;
+		token = strtok(NULL, "\t\n");
+		i++;
+	}
+		array[i] = NULL;
+		child_pid = fork();
 
-                if (child_pid == 0)
-                {
-                        if (execve (array[0], array, NULL) == -1)
-                                perror("Error:");
-                }
-                else
-                {
-                        wait(&status);
-                }
-                i = 0;
-                free(array);
-        }
+	if (child_pid == 0)
+	{
+		if (execve (array[0], array, NULL) == -1)
+			perror("Error:");
+    }
+	else
+	{
+        wait(&status);
+	}
+	i = 0;
+	free(array);
+    }
+return (0);
 }
