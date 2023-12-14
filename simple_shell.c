e9791fdd5ede770e4009fd08f931f9a9db03a383
Artiz58 <artiz58@gmail.com>
2023-12-13 21:58:29 -0400

    New modes to resolve simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,72 +1,118 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "shell.h"
 
-#define BUFFER_SIZE 1024
+int main(void)
+{
+    char line[MAX_LINE_LENGTH];
+    char **args;
+    char *copied_file;
+    pid_t pid;
 
-void prompt() {
-  printf("#cisfun$ ");
+    while (1)
+    {
+        /* Display prompt */
+        printf("> ");
+
+        /* Read user input */
+        if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL)
+        {
+            /* Handle end of file (Ctrl+D) */
+            printf("\n");
+            break;
+        }
+
+        line[strcspn(line, "\n")] = '\0'; /* Remove trailing newline */
+
+        if (!strcmp(line, ""))
+        { /* Case: Spaces only (small/large/medium) */
+            continue;
+        }
+
+        /* Parse command */
+        args = parse_command(line);
+
+        /* Check for exit command */
+        if (!strcmp(args[0], "exit"))
+        {
+            free(args);
+            break;
+        }
+
+        /* Handle special case: copy and execute */
+        if (!strcmp(args[0], "cp"))
+        {
+            int ret = system(line); /* Copy using system() */
+            if (ret != 0)
+            {
+                perror("system");
+                free(args);
+                continue;
+            }
+
+            /* Extract copied filename and remaining arguments */
+            copied_file = strtok(line + 3, " ");
+            args[0] = strdup(copied_file);
+        }
+
+        /* Fork a child process */
+        pid = fork();
+
+        if (pid == 0)
+        { /* Child process */
+            /* Execute command with arguments */
+            execvp(args[0], args);
+            perror(args[0]);
+            free(args);
+            exit(1);
+        }
+        else if (pid == -1)
+        { /* Error */
+            perror("fork");
+            free(args);
+            exit(1);
+        }
+        else
+        { /* Parent process */
+            waitpid(pid, NULL, 0); /* Wait for child to finish */
+            free(args);
+        }
+    }
+
+    return 0;
 }
 
-extern char **environ;
+/* Implementation of parse_command function */
+char **parse_command(char *line)
+{
+    int buffer_size = MAX_LINE_LENGTH;
+    int position = 0;
+    char **tokens = malloc(buffer_size * sizeof(char *));
+    char *token;
 
-int main() {
-  char line[BUFFER_SIZE];
-  char *args[2];
-  int status;
-  pid_t pid;
-
-  args[0] = line;
-  args[1] = NULL;
-
-  while (1) {
-    prompt();
-
-    /* Read the command line */
-    if (fgets(line, BUFFER_SIZE, stdin) == NULL) {
-      /* Handle Ctrl+D */
-      if (feof(stdin)) {
-        printf("\n");
-        break;
-      } else {
-        perror("fgets");
-        exit(1);
-      }
+    if (!tokens)
+    {
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
     }
 
-    /* Remove trailing newline character */
-    line[strcspn(line, "\n")] = '\0';
+    token = strtok(line, " ");
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
 
-    /* Check for empty command */
-    if (strlen(line) == 0) {
-      continue;
+        if (position >= buffer_size)
+        {
+            buffer_size += MAX_LINE_LENGTH;
+            tokens = realloc(tokens, buffer_size * sizeof(char *));
+            if (!tokens)
+            {
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, " ");
     }
-
-    /* Check for built-in command (currently only "exit") */
-    if (strcmp(line, "exit") == 0) {
-      break;
-    }
-
-    /* Fork a child process */
-    pid = fork();
-
-    if (pid == -1) {
-      perror("fork");
-      exit(1);
-    }
-
-    if (pid == 0) {
-      /* Child process */
-      execve(line, args, environ);
-      perror("execve");
-      exit(1);
-    } else {
-      /* Parent process */
-      waitpid(pid, &status, 0);
-    }
-  }
-
-  return 0;
+    tokens[position] = NULL;
+    return tokens;
 }
