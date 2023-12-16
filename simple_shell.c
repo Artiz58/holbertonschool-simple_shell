08bb207877d3ca45d82dece2f42b8dc1be3d55c7
Artiz58 <artiz58@gmail.com>
2023-12-15 23:10:32 -0400

    New Code

--- simple_shell.c
+++ simple_shell.c
@@ -4,80 +4,72 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define BUFFER_SIZE 256
-#define NUM_PATH_ENTRIES 5
+#define MAX_BUFFER_SIZE 1024
+#define MAX_ARGS 64
 
-extern char **environ;
+void read_command(char **command) {
+    size_t bufsize = 0;
+    ssize_t read_bytes;
+
+    printf("$ ");
+    read_bytes = getline(command, &bufsize, stdin);
+
+    if (read_bytes == -1) {
+        perror("getline");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Remove newline character if present */
+    if ((*command)[read_bytes - 1] == '\n') {
+        (*command)[read_bytes - 1] = '\0';
+    }
+}
+
+void execute_command(char *command) {
+    char *args[MAX_ARGS];
+    size_t arg_count = 0;
+    pid_t pid;
+
+    /* Tokenize the command */
+    char *token = strtok(command, " ");
+    while (token != NULL && arg_count < MAX_ARGS - 1) {
+        args[arg_count++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL; /* Null-terminate the argument list */
+
+    pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        /* Child process */
+        char *envp[] = { NULL }; /* You can modify this to include environment variables if needed */
+        execve(args[0], args, envp);
+        perror("execve");  /* Will only be reached if execve fails */
+        exit(EXIT_FAILURE);
+    } else {
+        /* Parent process */
+        wait(NULL);  /* Wait for the child to finish */
+    }
+}
 
 int main(void) {
-  char buffer[BUFFER_SIZE];
-  char *command;
-  char *args[BUFFER_SIZE];
-  int i = 0;
-  int j;
-  int command_found = 0;
-  char *full_path;
-  char *path_entries[NUM_PATH_ENTRIES] = {
-    "/usr/local/bin/",
-    "/usr/bin/",
-    "/bin/",
-    "/usr/local/games/",
-    "/usr/games/"
-  };
+    char *command = NULL;
 
-  while (1) {
-    /* Print the prompt */
-    printf("($) ");
-    fflush(stdout);
+    while (1) {
+        read_command(&command);
 
-    /* Read the command */
-    if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
-      /* Handle end of file (Ctrl+D) */
-      printf("\nExiting...\n");
-      break;
-    }
-
-    /* Set the command to "/bin/ls" */
-    command = "/bin/ls";
-
-    /* Tokenize the command and populate the args array */
-    args[0] = strtok(command, " ");
-    i = 1;
-    while ((args[i++] = strtok(NULL, " ")) != NULL && i < BUFFER_SIZE - 1);
-
-    /* Search for the command in the specified path entries */
-    for (j = 0; j < NUM_PATH_ENTRIES; ++j) {
-      /* Build the full path */
-      full_path = malloc(strlen(path_entries[j]) + strlen(args[0]) + 2);
-      if (full_path == NULL) {
-        perror("Error");
-        exit(EXIT_FAILURE);
-      }
-      strcpy(full_path, path_entries[j]);
-      strcat(full_path, "/");
-      strcat(full_path, args[0]);
-
-      /* Check if the file exists and is executable */
-      if (access(full_path, X_OK) == 0) {
-        /* Execute the command */
-        if (fork() == 0) {
-          execve(full_path, args, environ);
-          perror("Error");
-          exit(EXIT_FAILURE);
-        } else {
-          wait(NULL);
-          command_found = 1;
-          break;
+        /* Check for exit command */
+        if (strcmp(command, "exit") == 0) {
+            free(command);
+            printf("Exiting shell...\n");
+            exit(EXIT_SUCCESS);
         }
-      }
-      free(full_path);
+
+        execute_command(command);
     }
 
-    /* The command was not found in the specified paths */
-    if (!command_found) {
-      printf("Command not found: %s\n", args[0]);
-    }
-  }
-
-  return 0;
+    return 0;
 }
